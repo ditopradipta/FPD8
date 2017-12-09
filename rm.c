ff0e0fd3a6c3b3ff1fe6aba1fd6da6e5a43f63f4
nidarf <32903561+nidarf@users.noreply.github.com>
2017-12-09 11:32:29 +0700

    Update rm.c

--- rm.c
+++ rm.c
@@ -1,6 +1,7 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
+#include <syscall.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int
 main(int argc, char *argv[])
@@ -8,16 +9,30 @@ main(int argc, char *argv[])
   int i;
 
   if(argc < 2){
-    printf(2, "Usage: rm files or rm-r\n");
-    exit();
+    fprintf(stderr, "Usage: rm (file apa yg mau didelete?)\n");
+    sysexit();
   }
+
+  //buat hapus folder(di terminal jalan, di xv6 ga ngaruh->bisa hapus folder tanpa -r)
+if (strcmp(argv[1], "-r") == 0 || strcmp(argv[1], "-R") == 0) { // with -r
+    if (argc != 3) { // wrong usage
+      fprintf(stderr, "rm: terlalu banyak argumen\n");
+      sysexit();
+    } else { // remove folder yang gaada isinya
+      if (remove(argv[2]) < 0) {
+        fprintf(stderr, "rm: %s bukan direktori kosong\n", argv[2]);
+        sysexit();
+      }
+    }
+  }
+else
 
   for(i = 1; i < argc; i++){
     if(unlink(argv[i]) < 0){
-      printf(2, "rm: %s failed to delete\n", argv[i]);
+      fprintf(stderr, "rm: %s gagal dihapus\n", argv[i]);
       break;
     }
   }
 
-  exit();
+  sysexit();
 }
